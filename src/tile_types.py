c641289a5e0f111824bdbb4129b790419d3c2af7
kvnbanunu <kvnbanunu@gmail.com>
2024-07-08 03:25:15 -0700

    Implemented Field of View for the player and update map

--- src/tile_types.py
+++ src/tile_types.py
@@ -16,7 +16,8 @@ tile_dt = np.dtype(
     [
         ("walkable", np.bool),  # True if this tile can be walked over.
         ("transparent", np.bool),  # True if this tile doesn't block FOV.
-        ("rgb", graphic_dt),  # Graphics for when this tile is not in FOV.
+        ("dark", graphic_dt),  # Graphics for when this tile is not in FOV.
+        ("light", graphic_dt), # Graphics for when the tile is in FOV.
     ]
 )
 
@@ -25,20 +26,25 @@ def new_tile(
     *,  # Enforce the use of keywords, so that parameter order doesn't matter.
     walkable: int,
     transparent: int,
-    rgb: Tuple[int, Tuple[int, int, int], Tuple[int, int, int]],
+    dark: Tuple[int, Tuple[int, int, int], Tuple[int, int, int]],
+    light: Tuple[int, Tuple[int, int, int], Tuple[int, int, int]],
 ) -> np.ndarray:
     """Helper function for defining individual tile types"""
-    return np.array((walkable, transparent, rgb), dtype=tile_dt)
+    return np.array((walkable, transparent, dark, light), dtype=tile_dt)
 
+# SHROUD represents unexplored, unseen tiles
+SHROUD = np.array((ord(" "), (255, 255, 255), (0, 0, 0)), dtype=graphic_dt)
 
 floor = new_tile(
     walkable=True,
     transparent=True,
-    rgb=(ord(" "), (255, 255, 255), (50, 50, 150)),
+    dark=(ord("."), (100, 100, 100), (0, 0, 0)),
+    light=(ord("."), (200, 200, 200), (0, 0, 0)),
 )
 
 wall = new_tile(
     walkable=False,
     transparent=False,
-    rgb=(ord(" "), (255, 255, 255), (0, 0, 100)),
+    dark=(ord("#"), (100, 100, 100), (0, 0, 0)),
+    light=(ord("#"), (200, 200, 200), (0, 0, 0)),
 )
