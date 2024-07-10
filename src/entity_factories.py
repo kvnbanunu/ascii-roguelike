310da01d027e9683d223f58771933965d44db6f6
kvnbanunu <kvnbanunu@gmail.com>
2024-07-10 16:09:33 -0700

    Implemented movement pathing AI for enemies

--- src/entity_factories.py
+++ src/entity_factories.py
@@ -1,23 +1,28 @@
-from entity import Entity
+from components.ai import HostileEnemy
+from components.fighter import Fighter
+from entity import Actor
 
 
-player = Entity(
+player = Actor(
     char="@",
     color=(255, 255, 255),
     name="Player",
-    blocks_movement=True,
+    ai_cls=HostileEnemy,
+    fighter=Fighter(hp=30, defense=2, power=5),
 )
 
-goblin = Entity(
+goblin = Actor(
     char="g",
     color=(0, 127, 0),
     name="Goblin",
-    blocks_movement=True,
+    ai_cls=HostileEnemy,
+    fighter=Fighter(hp=10, defense=0, power=3),
 )
 
-orc = Entity(
+orc = Actor(
     char="o",
     color=(63, 127, 63),
     name="Orc",
-    blocks_movement=True,
+    ai_cls=HostileEnemy,
+    fighter=Fighter(hp=16, defense=1, power=4)
 )
